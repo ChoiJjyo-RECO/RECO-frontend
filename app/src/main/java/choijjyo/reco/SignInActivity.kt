6e707374499751bf6228089942b346d7c11ca734
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-05-04 14:21:18 +0900

    Feat: Splash, SignIn, SignUp 컴포넌트 생성

---
+++ app/src/main/java/choijjyo/reco/SignInActivity.kt
@@ -0,0 +1,13 @@
+package choijjyo.reco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/choijjyo/reco/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package choijjyo.reco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
