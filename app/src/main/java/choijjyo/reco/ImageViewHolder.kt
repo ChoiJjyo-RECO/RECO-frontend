bcd616e0c258c60e09d2a330f0d29b09a1132169
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-05-11 05:48:58 +0900

    Feat: 최근 스캔 사진 불러오기

---
+++ app/src/main/java/choijjyo/reco/ImageViewHolder.kt
@@ -0,0 +1,9 @@
+package choijjyo.reco
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val imageView: ImageView = itemView.findViewById(R.id.imageView)
+}
