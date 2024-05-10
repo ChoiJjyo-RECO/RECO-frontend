15dba76b7e60b1bc991c51160e86efb52f28840f
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-05-11 06:42:27 +0900

    Refactor: firestore 사진 불러오기 함수 리팩토링

--- app/src/main/java/choijjyo/reco/MainActivity.kt
+++ app/src/main/java/choijjyo/reco/MainActivity.kt
@@ -3,14 +3,11 @@ package choijjyo.reco
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import choijjyo.reco.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 
 class MainActivity : AppCompatActivity() {
 
@@ -64,11 +61,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        FirestoreHelper.loadImagesFromFirestore(this, "userUid", recyclerView)
+        FirestoreHelper.loadImagesFromFirestore(this, uid, recyclerView)
     }
-
-
-
-
 
 }
