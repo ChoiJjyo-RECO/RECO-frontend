5b2b52f67755f173220d2b9411cb9e13a9c88fd2
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-05-12 02:19:41 +0900

    Chore: opencv 관련 gitignore 추가

--- opencv/build/intermediates/cxx/Debug/37691c3j/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\AndroidStudioProjects\\RECO-frontend\\opencv\\.cxx\\Debug\\37691c3j\\arm64-v8a" ^
-  opencv_jni_shared
