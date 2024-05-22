3da37c47880d4890cce64530169bf35c174e0581
JoJosmin <ead3070@naver.com>
2024-05-22 17:42:41 +0900

    서버 주소 변경

--- app/src/main/java/choijjyo/reco/view/recognize/RecognizeActivity.kt
+++ app/src/main/java/choijjyo/reco/view/recognize/RecognizeActivity.kt
@@ -232,7 +232,7 @@ class RecognizeActivity : AppCompatActivity() {
             val docId = uri?.lastPathSegment
 
             // 요청 URL에 쿼리 매개변수 추가
-            val url = URL("https://c055-121-166-22-33.ngrok-free.app/detect_and_analyze?uid=$uid&doc_id=$docId")
+            val url = URL("https://4c67-121-166-22-33.ngrok-free.app/detect_and_analyze?uid=$uid&doc_id=$docId")
             val conn = url.openConnection() as HttpURLConnection
             conn.connectTimeout = 10000
             conn.requestMethod = "GET"
